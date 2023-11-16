f677f86af5468e9b4e74bf3fa4f68871dd95c5a4
JaroMasta <jaroslawmastalerczyk@gmail.com>
2023-11-16 21:21:53 +0100

    dodanie symulacji

---
+++ simulation.py
@@ -0,0 +1,10 @@
+import pygame
+
+class variables:
+    max_velocity = 100
+    movement_velocity = 7
+    frame_rate = 60
+    screen_size = (1280, 720)
+
+class key_bindings:
+    key_up = pygame.K_w
