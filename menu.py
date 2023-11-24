92d2e7563b9362ecbddf7e9845ee6bfc327cd59f
Natalia <147085553+nxtalia@users.noreply.github.com>
2023-11-24 18:19:08 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -5,20 +5,4 @@ import level
 
 from collections import defaultdict
 
-def count_character_frequency(text):
-    frequency = defaultdict(int)
-    for char in text:
-        if 'A' <= char <= 'z':
-            frequency[char] += 1
-    return frequency
-
-if __name__ == "__main__":
-    input_text = input("Wprowadź tekst: ")
-    character_frequency = count_character_frequency(input_text)
-    
-    for char, count in character_frequency.items():
-        print(f"Litera '{char}' wystąpiła {count} razy.")
-
-
-print("Zmiana wprowadzona przez Konrada!")
-
+#Test
