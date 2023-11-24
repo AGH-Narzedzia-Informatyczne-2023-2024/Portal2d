cf7f9639a2573c0ec4ecd1f7121e629434b874ea
JaroMasta <147257065+JaroMasta@users.noreply.github.com>
2023-11-24 17:41:22 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -3,28 +3,21 @@ import simulation
 import entities
 import level
 
-Konflikt_jarek
-main
-print  ("zmiana")
-main
+from collections import defaultdict
 
-Konflikt_Jarek
+def count_character_frequency(text):
+    frequency = defaultdict(int)
+    for char in text:
+        if 'A' <= char <= 'z':
+            frequency[char] += 1
+    return frequency
 
-print("Welcome to this beautiful world of git and python")
-
- main
-#Konflikt genrator2
-#Yaaaay 
-
-class menu:
-    def blit(self):
-        pass
+if __name__ == "__main__":
+    input_text = input("Wprowadź tekst: ")
+    character_frequency = count_character_frequency(input_text)
     
-    def button(self):
-        pass
-
-Konflikt_Bartek
-main
+    for char, count in character_frequency.items():
+        print(f"Litera '{char}' wystąpiła {count} razy.")
 
 
 print("Zmiana wprowadzona przez Konrada!")
